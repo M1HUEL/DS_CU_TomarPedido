630f66fcdfe0edf3b042185156ecb7fbe8818290
M1HUEL <imiguelx008@gmail.com>
2025-11-13 19:30:54 -0600

    🟢 Agregar clase de utilidades para los colores

---
+++ presentacion_equipo2/src/main/java/com/itson/presentacion/util/Colores.java
@@ -0,0 +1,14 @@
+package com.itson.presentacion.util;
+
+import java.awt.Color;
+
+public class Colores {
+
+    public static final Color NARANJA = new Color(255, 140, 0);
+    public static final Color NARANJA_CLARO = new Color(255, 183, 77);
+    public static final Color NARANJA_MUY_CLARO = new Color(255, 250, 240);
+    public static final Color CREMA = new Color(254, 249, 239);
+    public static final Color BLANCO = Color.WHITE;
+    public static final Color GRIS_BORDE = new Color(230, 230, 230);
+
+}
