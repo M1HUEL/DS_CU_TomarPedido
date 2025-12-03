a45a7ced77c89a7acef6e84e0114d22b90912099
M1HUEL <imiguelx008@gmail.com>
2025-12-03 03:19:24 -0600

    👍 Agregar constructor vacío al PersistenciaException

--- negocios-persistencia/src/main/java/com/itson/persistencia/exception/PersistenciaException.java
+++ negocios-persistencia/src/main/java/com/itson/persistencia/exception/PersistenciaException.java
@@ -1,17 +1,21 @@
 package com.itson.persistencia.exception;
 
 public class PersistenciaException extends Exception {
-    
+
+    public PersistenciaException() {
+        super();
+    }
+
     public PersistenciaException(String mensaje) {
         super(mensaje);
     }
-    
+
     public PersistenciaException(Throwable causa) {
         super(causa);
     }
-    
+
     public PersistenciaException(String mensaje, Throwable causa) {
         super(mensaje, causa);
     }
-    
+
 }
