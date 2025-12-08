ef7fa59aed01954a84d21ef67169dab5f548bd1c
M1HUEL <imiguelx008@gmail.com>
2025-12-07 22:46:46 -0600

    ❌ Eliminar clase y cambios menores

--- util/src/main/java/com/itson/util/sesion/Sesion.java
+++ util/src/main/java/com/itson/util/sesion/Sesion.java
@@ -4,14 +4,12 @@ import com.itson.persistencia.dominio.Usuario;
 
 public class Sesion {
 
-    private static Sesion instancia; // La única instancia
-    private Usuario usuarioLogueado; // El dato que queremos compartir
+    private static Sesion instancia;
+    private Usuario usuarioLogueado;
 
-    // Constructor privado para que nadie pueda hacer "new Sesion()"
     private Sesion() {
     }
 
-    // Método estático para obtener la instancia global
     public static Sesion getInstancia() {
         if (instancia == null) {
             instancia = new Sesion();
@@ -19,7 +17,6 @@ public class Sesion {
         return instancia;
     }
 
-    // Métodos para guardar y obtener el usuario
     public void setUsuarioLogueado(Usuario usuario) {
         this.usuarioLogueado = usuario;
     }
@@ -28,12 +25,10 @@ public class Sesion {
         return usuarioLogueado;
     }
 
-    // Método para cerrar sesión
     public void cerrarSesion() {
         this.usuarioLogueado = null;
     }
 
-    // Método utilitario para verificar si hay alguien
     public boolean haySesionActiva() {
         return this.usuarioLogueado != null;
     }
